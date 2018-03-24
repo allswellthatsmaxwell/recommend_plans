fb137b6f9269c6ec6b06f69859e01bef71e29913
allswellthatsmaxwell <allswellthatsmaxwell9@gmail.com>
2018-03-24 03:51:34 -0500

    changed sql queries to read into dataframe instead of tuple list

--- py/recommend_plans.py
+++ py/recommend_plans.py
@@ -1,13 +1,15 @@
 import os
 os.chdir("/home/mson/home/bind/recommend_plan")
 import numpy as np
+import pandas as pd
 import sqlite3
 
 def pull_table(conn, table):
-    return conn.execute(f"select * from {table}").fetchall()
+    return pd.read_sql_query(f"select * from {table}", conn)
 
 conn = sqlite3.connect("bind.db")
 services = pull_table(conn, "services")
 plans    = pull_table(conn, "plans")
 members  = pull_table(conn, "members")
 searches = pull_table(conn, "searches")
+conn.close()
